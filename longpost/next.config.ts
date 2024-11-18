f06ec065cf7f4238b4fde0f1d863eea07f58984d
mohit sharma <mohitlakshya@gmail.com>
2024-11-18 13:47:18 +0530

    fixing output path

--- longpost/next.config.ts
+++ longpost/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 export const isLocal = process.env.NODE_ENV === 'production'
-const basePath = isLocal ? '/bsky-projects/longpost' : path.join(process.env.HOME as string, 'github', 'bsky-projects', 'longpost', 'out')
+const basePath = isLocal ? '/bsky-projects' : path.join(process.env.HOME as string, 'github', 'bsky-projects', 'longpost', 'out')
 
 const nextConfig: NextConfig = {
   output: 'export',  // Enable static HTML export
