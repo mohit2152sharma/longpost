b8dd4d0e273e0e8547aadfb93f8cd05ac0190b2e
Mohit Sharma <mohitlakshya@gmail.com>
2024-11-23 20:56:33 +0530

    added few more buttons (#3)
    - added a shoutout button
    - added a support me button
    
    open links in new tab

--- longpost/tests/post.test.ts
+++ longpost/tests/post.test.ts
@@ -3,12 +3,12 @@ import { createThread } from "~/lib/post";
 describe("Test post functions", () => {
   const testCases = [
     {
-      text: "hello world",
+      parsedValues: { content: "hello world", shoutout: false },
       characterLimit: 5,
       expected: { error: false, message: "", texts: ["hello", "world"] },
     },
     {
-      text: "hello world!",
+      parsedValues: { content: "hello world!", shoutout: true },
       characterLimit: 5,
       expected: {
         error: true,
@@ -17,12 +17,15 @@ describe("Test post functions", () => {
       },
     },
     {
-      text: "hello world",
+      parsedValues: { content: "hello world", shoutout: false },
       characterLimit: 6,
       expected: { error: false, message: "", texts: ["hello", "world"] },
     },
     {
-      text: "hello     world world world world world world",
+      parsedValues: {
+        content: "hello     world world world world world world",
+        shoutout: false,
+      },
       characterLimit: 10,
       expected: {
         error: false,
@@ -31,7 +34,7 @@ describe("Test post functions", () => {
       },
     },
     {
-      text: "hello    world \n\n\nworld",
+      parsedValues: { content: "hello    world \n\n\nworld", shoutout: false },
       characterLimit: 11,
       expected: {
         error: false,
@@ -39,12 +42,30 @@ describe("Test post functions", () => {
         texts: ["hello (1/3)", "world (2/3)", "world (3/3)"],
       },
     },
+    {
+      parsedValues: { content: "hello    world \n\n\nworld", shoutout: true },
+      characterLimit: 11,
+      expected: {
+        error: false,
+        message: "",
+        texts: [
+          "hello (1/4)",
+          "world (2/4)",
+          "world (3/4)",
+          "This thread was created by Longpost. Try it out at: https://mohit2152sharma.github.io/bsky-projects/ (4/4)",
+        ],
+      },
+    },
   ];
 
   test.each(testCases)(
     "Testing thread function",
-    ({ text, characterLimit, expected }) => {
-      const { texts, error, message } = createThread(text, characterLimit);
+    ({ parsedValues, characterLimit, expected }) => {
+      const { texts, error, message } = createThread(
+        parsedValues.content,
+        parsedValues.shoutout,
+        characterLimit,
+      );
       expect(error).toBe(expected.error);
       expect(message).toBe(expected.message);
       expect(texts).toEqual(expected.texts);
