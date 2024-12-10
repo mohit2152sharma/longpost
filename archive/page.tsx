947ae0132e514880d345b6f103dd095336f3dc9b
mohit sharma <mohitlakshya@gmail.com>
2024-12-10 13:42:18 +0530

    removing archive as it is no longer needed

--- archive/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function HomePage() {
-	return (
-		<div className="container mx-auto">
-			<h1 className="my-4 text-2xl font-bold">Welcome to the Landing Page</h1>
-			<p className="text-lg">You have successfully logged in to Bluesky!</p>
-		</div>
-	);
-}
--- archive/src/components/editor/editor-types.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const contentSchema = z.object({
-	content: z.string(),
-	shoutout: z.boolean().default(true)
-});
-
-export type Content = z.infer<typeof contentSchema>;
