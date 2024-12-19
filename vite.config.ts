21f7836b1d875226eff2f74bee013c45b95d67e7
mohit sharma <mohitlakshya@gmail.com>
2024-12-19 19:03:44 +0530

    adding tests to ci

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
 	plugins: [sveltekit()],
 
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			reporter: ['text', 'json-summary', 'json'],
+			reportOnFailure: true
+		}
 	}
 });
