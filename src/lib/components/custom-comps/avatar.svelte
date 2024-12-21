3d87a2744467cdecb3dacd1b8e20de74d104adbd
mohit sharma <mohitlakshya@gmail.com>
2024-12-21 18:18:33 +0530

    - do not modify prop in a svelte component

--- src/lib/components/custom-comps/avatar.svelte
+++ src/lib/components/custom-comps/avatar.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import * as Avatar from '$lib/components/ui/avatar/index.js';
 	export let userName: string = 'BS';
+	let avatar: string
 
 	if (userName.length > 2) {
-		userName = userName.slice(0, 2).toUpperCase();
+		avatar = userName.slice(0, 2).toUpperCase();
 	} else {
-		userName = userName.toUpperCase();
+		avatar = userName.toUpperCase();
 	}
 </script>
 
 <Avatar.Root>
 	<!-- <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" /> -->
-	<Avatar.Fallback>{userName}</Avatar.Fallback>
+	<Avatar.Fallback>{avatar}</Avatar.Fallback>
 </Avatar.Root>
